7ef3aa8c8ce78191d23f14d2ada5910ec8d18a81
bilginyuksel <bilgin.yuksel96@gmail.com>
2020-11-28 11:04:43 +0300

    util static string changed. Asking change requests for TSC classes

--- cordova-plugin-hms-example/src/android/basef/handler/TSCGenerator.java
+++ cordova-plugin-hms-example/src/android/basef/handler/TSCGenerator.java
@@ -14,63 +14,37 @@ public class TSCGenerator {
     private final List<String> moduleReferences = new ArrayList<>();
 
     public static final String TS_UTILS_FILE = "import {exec} from 'cordova';\n" +
-            "\n" +
-            "export function asyncExec (clazz: string, ref: string, args: any[]=[]) : Promise<any>{\n" +
-            "    return new Promise<any>((resolve, reject) => {\n" +
-            "        exec(resolve, reject, clazz, ref, args);\n" +
-            "    });\n" +
-            "}\n" +
-            "\n" +
-            "type Handler = (data: any) => void;\n" +
-            "\n" +
-            "declare global {\n" +
-            "    interface Window {\n" +
-            "        hmsEventHandlers: {\n" +
-            "            [key: string]: Handler[]\n" +
-            "        },\n" +
-            "        hmsEventHandler: (eventName: string, data: any) => void,\n" +
-            "        registerHMSEvent: (eventName: string, handler: Handler) => void,\n" +
-            "        unregisterHMSEvent: (eventName: string, handler: Handler) => void,\n" +
-            "        [key: string]: any\n" +
-            "    }\n" +
-            "}\n" +
-            "\n" +
-            "initEventHandler();\n" +
-            "\n" +
-            "function initEventHandler() {\n" +
-            "    if (window.hmsEventHandler != null) {\n" +
-            "        return;\n" +
-            "    }\n" +
-            "\n" +
-            "    window.hmsEventHandlers = {};\n" +
-            "\n" +
-            "    window.hmsEventHandler = (eventName, data) => {\n" +
-            "        console.log('eventReceived: ' + eventName + ' with data: ', data);\n" +
-            "        if (window.hmsEventHandlers.hasOwnProperty(eventName)) {\n" +
-            "            window.hmsEventHandlers[eventName].forEach(handler => {\n" +
-            "                handler(data);\n" +
-            "            });\n" +
-            "        }\n" +
-            "    }\n" +
-            "\n" +
-            "    window.registerHMSEvent = (eventName, handler) => {\n" +
-            "        if (window.hmsEventHandlers.hasOwnProperty(eventName)) {\n" +
-            "            window.hmsEventHandlers[eventName].push(handler);\n" +
-            "        } else {\n" +
-            "            window.hmsEventHandlers[eventName] = [handler];\n" +
-            "        }\n" +
-            "    };\n" +
-            "\n" +
-            "    window.unregisterHMSEvent = (eventName, handler) => {\n" +
-            "        if (window.hmsEventHandlers.hasOwnProperty(eventName)) {\n" +
-            "            const idx = window.hmsEventHandlers[eventName].indexOf(handler);\n" +
-            "            if (idx > -1) {\n" +
-            "                window.hmsEventHandlers[eventName].splice(idx, 1);\n" +
-            "            }\n" +
-            "        }\n" +
-            "\n" +
-            "    };\n" +
-            "}";
+			"\n" +
+			"export function asyncExec(clazz: string, func: string, args:any[] = []): Promise<any> {\n" +
+			"    return new Promise((resolve, reject) => {\n" +
+			"        exec(resolve, reject, clazz, func, args);\n" +
+			"    });\n" +
+			"}\n" +
+			"\n" +
+			"declare global {\n" +
+			"    interface Window {\n" +
+			"        hmsEvents: {\n" +
+			"            [key: string]: (data: any) => void\n" +
+			"        },\n" +
+			"        runHMSEvent: (eventName: string, data: any) => void,\n" +
+			"        subscribeHMSEvent: (eventName: string, callback: (data: any) => void) => void\n" +
+			"        [key: string]: any\n" +
+			"    }\n" +
+			"}\n" +
+			"\n" +
+			"function initEventHandler() {\n" +
+			"    if (window.hmsEvents != null) return;\n" +
+			"    window.hmsEvents = {};\n" +
+			"    window.runHMSEvent = (eventName, data) => {\n" +
+			"        if (window.hmsEvents.hasOwnProperty(eventName))\n" +
+			"            window.hmsEvents[eventName](data);\n" +
+			"    };\n" +
+			"    window.subscribeHMSEvent = (eventName, handler) => {\n" +
+			"        window.hmsEvents[eventName] = handler;\n" +
+			"    };\n" +
+			"}\n" +
+			"\n" +
+			"initEventHandler()";
 
     public <T extends CordovaBaseModule> TSCGenerator(String pluginName, List<T> cordovaModules) {
         this.pluginName = pluginName;
@@ -112,7 +86,7 @@ public class TSCGenerator {
     private String generateEventMethod() {
         StringBuilder sb = new StringBuilder();
         sb.append("\n\ton(eventName: string, callback: (val: any) => void): void {\n");
-        sb.append("\t\twindow.registerHMSEvent(eventName, callback);\n");
+        sb.append("\t\twindow.subscribeHMSEvent(eventName, callback);\n");
         sb.append("\t}\n");
         return sb.toString();
     }
