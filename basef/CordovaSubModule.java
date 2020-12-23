b74de24c47405e8ed8e1e448b1ebf6189149fd8d
bilginyuksel <bilgin.yuksel96@gmail.com>
2020-12-23 09:21:59 +0300

    cordova base module departed from experimental work

--- basef/CordovaSubModule.java
+++
@@ -1,8 +0,0 @@
-package com.cor.android.framework.basef;
-
-public abstract class CordovaSubModule extends CordovaBaseModule {
-    public CordovaSubModule() {
-        super();
-        isSubModule = true;
-    }
-}
