d5b14a4d0dc7e7a774a32c605d790e916d523236
bilginyuksel <bilgin.yuksel96@gmail.com>
2020-12-23 09:23:31 +0300

    sub module experimental development started

--- basef/CordovaBaseModule.java
+++ basef/CordovaBaseModule.java
@@ -2,11 +2,17 @@ package com.cor.android.framework.basef;
 
 import android.content.Intent;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public abstract class CordovaBaseModule {
     private final String reference;
+    protected boolean isSubModule;
+    private final Map<String, CordovaSubModule> subModuleMap = new ConcurrentHashMap<>();
 
     public CordovaBaseModule() {
         this.reference = this.getClass().getSimpleName();
+        this.isSubModule = false;
     }
     public void onDestroy(){}
     public void onPause(boolean multitasking){}
@@ -17,8 +23,19 @@ public abstract class CordovaBaseModule {
     public void onActivityResult(int requestCode, int resultCode, Intent data){}
     public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults){}
 
+    public void addSubModule(CordovaSubModule subModule) {
+        subModuleMap.put(subModule.getReference(), subModule);
+    }
+
+    public Map<String, CordovaSubModule> getSubModuleMap() {
+        return subModuleMap;
+    }
+
     public String getReference(){
         return reference;
     }
 
+    public boolean isSubModule() {
+        return isSubModule;
+    }
 }
